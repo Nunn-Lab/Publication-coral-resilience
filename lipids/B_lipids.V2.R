dc88e0ac1096a57ae88e870db757e74fce76f455
Miranda Mudge <mudge832@uw.edu>
2023-02-01 15:04:56 -0800

    Update B_lipids.V2.R

--- lipids/B_lipids.V2.R
+++ lipids/B_lipids.V2.R
@@ -30,15 +30,51 @@ lipid.dat3$Toler.Tim.Stat <-paste(lipid.dat3$Tolerance,lipid.dat3$Time, lipid.da
 #I want RT1='#1b7837','ST1#762a83','Rt2=#7fbf7b','St2=#af8dc3' and NB samples to be behind the othe T2 samples in light grey
 #also want x labels to be T1 and T2 (to match others)
 #order of labels etc= RT1,Rt2,RT2NB,ST1,ST2,ST2NB
-ggplot(data=lipid.dat3,aes(x=Time, y=Lipid..Biomass..g.gdw., fill=Toler.Tim.Stat, color=Toler.Tim.Stat)) +
-  geom_boxplot() +
+
+#annotate colors
+outside = c('#95C38B','#1B7837','#1B7837','#8857A8','#762A83','#762A83')
+inside = c('#1b7837','grey90','#7fbf7b','#762a83','#FAFAFA','#af8dc3')
+
+lipid.dat4 <-lipid.dat3
+lipid.dat4$Toler.Tim.Stat <- as.factor(lipid.dat4$Toler.Tim.Stat)
+
+#function to calculate mean and standard deviation
+data_summary <- function(data, varname, groupnames){
+  require(plyr)
+  summary_func <- function(x, col){
+    c(mean = mean(x[[col]], na.rm=TRUE),
+      sd = sd(x[[col]], na.rm=TRUE))
+  }
+  data_sum<-ddply(data, groupnames, .fun=summary_func,
+                  varname)
+  data_sum <- rename(data_sum, c("mean" = varname))
+  return(data_sum)
+}
+
+#run function
+lipid.summary <- data_summary(lipid.dat4, varname="Lipid..Biomass..g.gdw.", 
+                    groupnames=c("Toler.Tim.Stat", "Status", "Time", "Tim.Stat"))
+
+lipid.summary$Time=as.factor(lipid.summary$Time)
+lipid.summary$Tim.Stat=as.factor(lipid.summary$Tim.Stat)
+
+#plot
+ggplot(data=lipid.summary,aes(x=Tim.Stat, y=Lipid..Biomass..g.gdw., fill=Toler.Tim.Stat)) +
+  geom_bar(aes(fill=Toler.Tim.Stat, color=Toler.Tim.Stat),
+           position="dodge", stat="identity") +
+  geom_errorbar(aes(ymin=Lipid..Biomass..g.gdw.-sd, ymax=Lipid..Biomass..g.gdw.+sd ), 
+                width=.2, position=position_dodge(.9), 
+                color=c('#95C38B','#762A83','#1B7837','#8857A8','#1B7837','#762A83')) +
   theme_classic(base_size = 20) +
-  scale_color_manual(values=c('#95C38B','#1B7837','#1B7837','#8857A8','#762A83','#762A83'), labels=c('Bleached', 'Non-bleached','bb','aa','cc','dd'))+
-  scale_fill_manual(values=c('#1b7837','grey90','#7fbf7b','#762a83','#FAFAFA','#af8dc3'), labels=c('RT1', 'RT2','ST1', 'ST2')) +
+  scale_color_manual(values=outside)+
+  scale_fill_manual(values=inside) +
   ylab('Lipid Biomass (g/g dry weight)') +
-  scale_x_discrete(labels=c('Timepoint 1', 'Timepoint 2', 'Timepoint 2NB')) +
+  xlab('Time') +
   facet_wrap(~Time*Status, scales='free_x') +
+  scale_x_discrete(breaks=c('0 none', '2 B', '2 NB'),
+                   labels=c('Timepoint 1', 'Timepoint 2', 'Timepoint 2NB')) +
   theme(strip.background=element_blank(), strip.text.x=element_blank())
+
 
 #c('#1b7837','#7fbf7b','#762a83','#af8dc3','#af8dc3','grey')
 png("~/Documents/genome_sciences_postdoc/corals/time series/Mcap T1T2 lipids.png", width=7, height=7, units = "in", res = 1200)
