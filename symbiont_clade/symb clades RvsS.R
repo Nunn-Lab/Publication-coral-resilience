76a1047a5953c0e0ded10092e211b169b6a743b3
brooknunn <brookh@uw.edu>
2024-12-19 10:53:55 -0800

    Update symb clades RvsS.R

--- symbiont_clade/symb clades RvsS.R
+++ symbiont_clade/symb clades RvsS.R
@@ -33,3 +33,46 @@ Bleaching:Sample..            2 0.0152 0.00762   0.139 0.8708
 Tolerance:Sample..            1 0.0737 0.07371   1.346 0.2574  
 Bleaching:Tolerance:Sample..  2 0.2711 0.13555   2.476 0.1053  
 Residuals                    24 1.3140 0.05475  
+
+
+# Logistic Regressin Analysis to determine if the proportion of Durusdinium:Cladocopium influence survivability
+# Input the data from symbiont proportions into dataframe
+data <- data.frame(
+  Coral_Genet_ID = c(5, 20, 25, 28, 55, 73, 10, 13, 21, 26, 66, 74),
+  Susceptibility = c("S", "S", "S", "S", "S", "S", "R", "R", "R", "R", "R", "R"),
+  Proportion_Cladocopium = c(0.196, 0.142, 0, 0.081, 0, NA, 0, 1, 0.843, 0, 0, 0),
+  Proportion_Durusdinium = c(0.804, 0.858, 1, 0.919, 1, NA, 1, 0, 0.157, 1, 1, 1)
+)
+
+# Remove rows with missing values
+data <- na.omit(data)
+
+# Convert 'Susceptibility' to a binary variable (1 for 'S', 0 for 'R')
+data$Susceptibility_Binary <- ifelse(data$Susceptibility == "S", 1, 0)
+
+# Perform logistic regression
+model <- glm(Susceptibility_Binary ~ Proportion_Durusdinium, data = data, family = binomial)
+
+# Display the summary of the model
+summary(model)
+
+
+Call:
+glm(formula = Susceptibility_Binary ~ Proportion_Durusdinium, family = binomial, data = data)
+
+Deviance Residuals: 
+   Min      1Q  Median      3Q     Max  
+-1.278  -0.886  -0.533   1.051   1.210  
+
+Coefficients:
+                        Estimate Std. Error z value Pr(>|z|)  
+(Intercept)              -2.1581     2.2870  -0.944   0.345  
+Proportion_Durusdinium    2.3817     2.5040   0.951   0.342  
+
+(Dispersion parameter for binomial family taken to be 1)
+
+    Null deviance: 15.162  on 10  degrees of freedom
+Residual deviance: 13.873  on  9  degrees of freedom
+AIC: 17.873
+
+Number of Fisher Scoring iterations: 6
